e0c5b9d8c9ddc31085d808ff4078f0f3b43eb82c
Shahar <shaharariel95@gmail.com>
2022-12-31 16:25:24 +0200

    change the checkPremissions function and moved it to validatorService.js

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const accountsRouter = new express.Router();
 const accountController = require('../controllers/accountsController');
+const { checkPermission } = require('../middleware/validatorService');
 
 accountsRouter.get('/invite/:email', accountController.inviteUser);
 accountsRouter.get('/:accountId/', accountController.getAccount);
 accountsRouter.put('/:accountId/', accountController.editAccount);
-accountsRouter.put('/disable/:accountId/', accountController.disableAccount);
+accountsRouter.put('/disable/:accountId/', checkPermission, accountController.disableAccount);
 
 module.exports = { accountsRouter };

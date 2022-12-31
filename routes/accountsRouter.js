0cfa6b2f04deeb0f974dfaefb4f40eea634d887a
Shahar <shaharariel95@gmail.com>
2022-12-31 23:38:45 +0200

    edit account works

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -5,9 +5,9 @@ const accountController = require('../controllers/accountsController');
 const { checkPermission, checkPermissionAdmin } = require('../middleware/validatorService');
 
 accountsRouter.get('/invite/:email', accountController.inviteUser); // done
-accountsRouter.get('/getAll/'/* , checkPermissionAdmin*/, accountController.getAccounts);
-accountsRouter.get('/:accountId/', accountController.getAccount); // done
-accountsRouter.put('/:accountId/', checkPermission, accountController.editAccount);
-accountsRouter.put('/disable/:accountId/', checkPermission, accountController.disableAccount);
+accountsRouter.get('/getAll/'/* , checkPermissionAdmin*/, accountController.getAccounts); // done
+accountsRouter.get('/:email/', accountController.getAccount); // done
+accountsRouter.put('/edit/'/* , checkPermission */, accountController.editAccount); // done
+accountsRouter.put('/disable/'/* , checkPermission */, accountController.disableAccount);
 
 module.exports = { accountsRouter };

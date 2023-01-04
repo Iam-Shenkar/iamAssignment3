aa1bd0576a182c197bf01a48d02f774fca91f259
ronina24 <roni24682@gmail.com>
2023-01-05 00:15:42 +0200

    assets

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/auth', validation, auth.authRouter);
-app.use('/assets', assets.assetsRoute);
+app.use('/assets',authenticateToken, assets.assetsRoute);
 app.use('/users', authenticateToken, users.usersRouter);
 app.use('/accounts', authenticateToken, accounts.accountsRouter);
 app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, './client/Login.html')); });

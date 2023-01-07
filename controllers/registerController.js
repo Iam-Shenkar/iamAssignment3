f2473e9d156a9558717cb239c12f562844f27535
dkracheli <83089578+dkracheli@users.noreply.github.com>
2023-01-07 16:30:23 +0200

    ra

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -9,9 +9,8 @@ const { httpError } = require('../class/httpError');
 
 const { freePlan2Q } = require('../Q/sender');
 
-
 const { Account, User } = require('../repositories/repositories.init');
-
+const { setSeats } = require('../services/assetsService');
 
 const handleRegister = async (req, res, next) => {
   try {
@@ -79,6 +78,7 @@ const confirmationUser = async (req, res, next) => {
     } else if (user.status === 'active') {
       await Account.delete({ _id: user.accountId });
       await User.update({ email }, { accountId });
+      await setSeats(accountId, 1);
     } else {
       throw new httpError(401, 'Unable to confirm this user');
     }
--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -4,8 +4,9 @@ const accountsRouter = new express.Router();
 const accountController = require('../controllers/accountsController');
 const { checkPermissionAdmin } = require('../middleware/validatorService');
 
+accountsRouter.get('/Bupgrade', accountController.upgrade);
 accountsRouter.get('/', checkPermissionAdmin, accountController.getAccounts); // done
-accountsRouter.get('/:account/link/:email', accountController.inviteUser); // done
+accountsRouter.get('/:accountId/link/:email', accountController.inviteUser); // done
 accountsRouter.get('/:id/', accountController.getAccount); // done
 accountsRouter.put('/:id', checkPermissionAdmin, accountController.editAccount); // done
 accountsRouter.put('/status/:id', checkPermissionAdmin, accountController.disableAccount);
